1847e1de10f7def3f43d4e4b950f057f29a5d91e
rickfelipe20-lgtm <rick.felipe20@gmail.com>
2026-02-12 11:59:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Header transparente ao rolar
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 20) header.style.background = "#ffffffee";
-    else header.style.background = "#ffffffcc";
-});
